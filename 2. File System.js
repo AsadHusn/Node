1e951bc30c705ab4cf6b300a77959e2e53544316
Asad <asadhusn007@gmail.com>
2022-11-15 03:37:01 +0500

    changes

--- 2. File System.js
+++ 2. File System.js
@@ -1,11 +1,46 @@
 fs = require('fs')
 
-// synchronous call
-folder = fs.readdirSync('./')
+// * READ DIRECTORY
+// folder = fs.readdirSync('./')
+// console.log(folder)
 
+// fs.readdir('./', (err, files)=> {
+//     console.log(err ? err : files)
+// })
 
-// async call
-folder = fs.readdir('./', (error, files)=> {
-    if(error) console.log(error)
-    else console.log(files)
-})
+// * READ FILE
+// file = fs.readFileSync('index.html', 'UTF-8')
+// console.log(file)
+
+// fs.readFile('index.html', 'UTF-8', (err, data)=>{
+//     console.log(err ? err : data)
+// })
+
+// * APPEND FILE
+// fs.appendFileSync('file.txt', "hello world")
+
+// fs.appendFile('file.txt', 'hey', err=>{
+//     if(err) throw err
+// })
+
+// * WRITE FILE
+// fs.writeFileSync('file.txt', 'Hello Asad')
+
+// fs.writeFile('file.txt', "hey asad", err=>{
+//     if(err) throw err
+// })
+
+// * DELETE FILE
+// fs.unlinkSync('file.txt')
+
+// fs.unlink('file.txt', (err)=>{
+//     if(err) throw err
+// })
+
+// * RENAME FILE
+// fs.renameSync('file.txt', 'text.txt')
+
+// fs.rename('text.txt', 'file.txt', (err)=>{
+//     if(err) throw err
+// })
+
--- app.js
+++
@@ -1,3 +0,0 @@
-fun = require("./1. module")
-
-fun.fn()
